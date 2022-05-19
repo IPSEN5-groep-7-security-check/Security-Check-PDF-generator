cceb818d8b5271573b93b8fea924e74bd2fe7485
foekinator656 <loek@appel-snijder.nl>
2022-05-19 11:36:43 +0200

    begin pdf inplannen

--- form-to-pdf/routes/pdf.js
+++ form-to-pdf/routes/pdf.js
@@ -6,12 +6,16 @@ router.post('/', (req, res) => {
     const doc = new PDFDocument()
     let filename = req.body.filename
     // Stripping special characters
-    filename = encodeURIComponent(filename) + '.pdf'
+    // filename = encodeURIComponent(filename) + '.pdf'
+    let test = "een_pad_test"
+    filename = test + '.pdf';
     // Setting response to 'attachment' (download).
     // If you use 'inline' here it will automatically open the PDF
     res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
     res.setHeader('Content-type', 'application/pdf')
-    const content = req.body.content
+    // const content = req.body.content
+    const content = "appeltaart\n testb"
+
     doc.y = 300
     doc.text(content, 50, 50)
     doc.pipe(res)
